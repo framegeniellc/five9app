37ff607508083925c636a2e2d40536662838260c
webmariag <mguerra@nowoptics.com>
2020-11-30 16:03:45 -0600

    Store Information css styles

--- src/components/CustomerAcquisition/StoreInformation/Doctor/Doctor.tsx
+++ src/components/CustomerAcquisition/StoreInformation/Doctor/Doctor.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import { IDoctor } from '../../../interfaces/global'
+import css from './Doctor.module.scss'
 
 const Doctor = (props: any) => {
     const { doctorInfo } = props
 
     return (
-        <div>
-            <div>{doctorInfo.FirstName} {doctorInfo.LastName}</div>
-            <div>{doctorInfo.Comments && (<p>{doctorInfo.Comments}</p>)}</div>
+        <div className={css.doctors}>
+            <div className={css.bullet}>{doctorInfo.FirstName} {doctorInfo.LastName}</div>
+            {doctorInfo.Comments && (<div className={css.comments}><p>{doctorInfo.Comments}</p></div>)}
         </div>
     )
 }
