ce719604afca10875481fc49df559a46f1e2a08d
Juan Carlos Martinez Lopez <jumartinezlopez@newoptics.com>
2020-11-30 22:30:47 -0600

    Fix me: Set normal call behavior when IVR parameter is not set in url

--- src/components/CustomerAcquisition/Common/store.tsx
+++ src/components/CustomerAcquisition/Common/store.tsx
@@ -46,6 +46,10 @@ const getStoreScript = (IVR: string, store: any) => {
         case '2.1.3.1': { return getDefaultScript(store) }
     }
 
+    if(store && !IVR) {
+        return getDefaultScript(store)
+    }
+
     return `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
 }
 
