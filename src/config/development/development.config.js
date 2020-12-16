c869741d9c276d4858cf39132fbcb1e5192dca74
Juan Carlos Martinez Lopez <jumartinezlopez@newoptics.com>
2020-12-16 09:39:47 -0600

    Change endpoint url

--- src/config/development/development.config.js
+++ src/config/development/development.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    NOBLE_ZEUS_URL: "http://testx.myeyelab.com/api",
+    NOBLE_ZEUS_URL: "https://testx.myeyelab.com/api",
     GMAP_KEY: "AIzaSyAT0SS9kZZQRJRdlHYF3fcZmlVvWN-3cfM",
 }
--- src/config/production/production.config.js
+++ src/config/production/production.config.js
@@ -1,4 +1,4 @@
 export default {
-    NOBLE_ZEUS_URL: "http://127.0.0.1:8000/api",
+    NOBLE_ZEUS_URL: "https://testx.myeyelab.com/api",
     GMAP_KEY: "AIzaSyAT0SS9kZZQRJRdlHYF3fcZmlVvWN-3cfM",
 }
